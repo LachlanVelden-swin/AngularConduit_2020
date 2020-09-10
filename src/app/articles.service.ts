d49738f46d887b098b8c9883f3eb31047f7fa6bf
LachlanVelden-swin <ldvandervelden@swin.edu.au>
2020-09-10 11:53:51 +1000

    add ArticlesService

---
+++ src/app/articles.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Article, ArticlesEnvelope } from './article';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticlesService {
+
+  articles: Article[] = [];
+  loading: boolean = false; 
+  loaded: boolean = false;
+
+  private httpClient: HttpClient;
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  /**
+   * Go and get an ArticlesEnvelope from the server and store it locally
+   */
+  get() {
+    let request = this.httpClient.get<ArticlesEnvelope>("https://swindev.me/articles");
+    this.loading = true;
+    this.loaded = false;
+    request.subscribe((response) => {
+      
+      this.articles = response.articles;
+      this.loading = false;
+      this.loaded = true;
+    });
+  }
+
+}
